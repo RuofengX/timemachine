969203128ccc33ac3f2eef4394a70b9aed4c6d82
ruofeng <weiruofeng@ruofengx.cn>
2022-05-17 23:34:22 +0800

    Add gitignore

--- src/main/java/cn/ruofengx/TimeMachine/TimeMachine.java
+++ src/main/java/cn/ruofengx/TimeMachine/TimeMachine.java
@@ -1,18 +1,26 @@
 package cn.ruofengx.TimeMachine;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TimeMachine extends JavaPlugin {
+public final class TimeMachine extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
        getLogger().info("This is my first timemachine, it still do nothing yet.");
+       Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable () {
-
-
+    @EventHandler
+    public static void onPlayerJoin(PlayerJoinEvent event) {
+        
+       event.getPlayer().sendMessage("Welcome");
+    
     }
 
+
+    
 }
