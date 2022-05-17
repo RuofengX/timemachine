f09d42e0f5c0ba26160684f1eb9187f5b678065b
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-17 16:28:02 +0800

    First commit

---
+++ src/main/java/cn/ruofengx/TimeMachine/TimeMachine.java
@@ -0,0 +1,18 @@
+package cn.ruofengx.TimeMachine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimeMachine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+       getLogger().info("This is my first timemachine, it still do nothing yet.");
+    }
+
+    @Override
+    public void onDisable () {
+
+
+    }
+
+}
