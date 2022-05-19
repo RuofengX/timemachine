180f2c694ff45ea44d4b270bcdc26c973bd4f00e
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-19 15:33:49 +0800

    correct a null point bug

--- src/main/java/cn/ruofengx/TimeMachine/SaveUploader.java
+++ src/main/java/cn/ruofengx/TimeMachine/SaveUploader.java
@@ -153,7 +153,7 @@ final class COSManager {
 
     public void Upload(File localFile, String prefix) {
         // 使用高级接口必须先保证本进程存在一个 TransferManager 实例
-        createTransferManager(this.REGION_NAME);
+        this.createTransferManager();
 
         // 存储桶的命名格式为 BucketName-APPID，此处填写的存储桶名称必须为此格式
         String bucketName = this.BUCKET_NAME;
@@ -201,25 +201,24 @@ final class COSManager {
     }
 
     // 创建 TransferManager 实例，这个实例用来后续调用高级接口
-    private TransferManager createTransferManager(String regionName) {
+    private void createTransferManager() {
         // 创建一个 COSClient 实例，这是访问 COS 服务的基础实例。
-        COSClient cosClient = createCOSClient(regionName);
+        COSClient cosClient = createCOSClient(this.REGION_NAME);
 
         // 自定义线程池大小，建议在客户端与 COS 网络充足（例如使用腾讯云的 CVM，同地域上传 COS）的情况下，设置成16或32即可，可较充分的利用网络资源
         // 对于使用公网传输且网络带宽质量不高的情况，建议减小该值，避免因网速过慢，造成请求超时。
         ExecutorService threadPool = Executors.newFixedThreadPool(32);
 
         // 传入一个 threadpool, 若不传入线程池，默认 TransferManager 中会生成一个单线程的线程池。
-        TransferManager transferManager = new TransferManager(cosClient, threadPool);
+        this.transferManager = new TransferManager(cosClient, threadPool);
 
         // 设置高级接口的配置项
         // 分块上传阈值和分块大小分别为 5MB 和 1MB
         TransferManagerConfiguration transferManagerConfiguration = new TransferManagerConfiguration();
         transferManagerConfiguration.setMultipartUploadThreshold(5 * 1024 * 1024);
         transferManagerConfiguration.setMinimumUploadPartSize(1 * 1024 * 1024);
-        transferManager.setConfiguration(transferManagerConfiguration);
+        this.transferManager.setConfiguration(transferManagerConfiguration);
 
-        return transferManager;
     }
 
     private void shutdownTransferManager(TransferManager transferManager) {
