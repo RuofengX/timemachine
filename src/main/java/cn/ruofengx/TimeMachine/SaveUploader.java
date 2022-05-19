6929fd6a13a82aa94e1e050fac4f8d20eb01a9af
weiruofeng <weiruofeng@ruofengx.cn>
2022-05-19 14:11:46 +0800

    add saveuploader function, untested

--- src/main/java/cn/ruofengx/TimeMachine/SaveUploader.java
+++ src/main/java/cn/ruofengx/TimeMachine/SaveUploader.java
@@ -1,54 +1,77 @@
 package cn.ruofengx.TimeMachine;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.UploadResult;
+import com.qcloud.cos.region.Region;
+import com.qcloud.cos.transfer.TransferManager;
+import com.qcloud.cos.transfer.TransferManagerConfiguration;
+import com.qcloud.cos.transfer.Upload;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.scheduler.BukkitRunnable;
+
 // 定时将游戏存档上传至COS存储
-public class SaveUploader {
-
-    private String API_TOKEN;
+public class SaveUploader extends BukkitRunnable {
+    private String API_ID;
     private String API_KEY;
+    private String REGION;
+    private String BUCKET_NAME;
+    private String folderPath;
+    private int interval;
 
-    public SaveUploader(String configFileName) {
-        getAPITokenFromFile(configFileName);
+
+    public SaveUploader(ConfigurationSection section) {
+        this.API_ID = section.getString("tencent-cos.secret-id");
+        this.API_KEY = section.getString("tencent-cos.secret-key");
+        this.REGION = section.getString("tencent-cos.region");
+        this.BUCKET_NAME = section.getString("tencent-cos.bucket");
+        this.folderPath = section.getString("world-folder");
+        this.interval = section.getInt("interval-second");
+
     }
 
-    public SaveUploader() {
-        this("TENCENT_KEY");
+    public int getInterval() {
+        return interval;
     }
 
-    private void getAPITokenFromFile(String fileName) {
+    @Override
+    public void run() {
+        // What you want to schedule goes here
+        this.SaveAndUpload();
+    }
+
+    private void SaveAndUpload() { // 全同步方法
         try {
-            File file = new File(fileName);
-            if (!file.exists()) {
-                throw new FileNotFoundException("API file not found");
-            }
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith("API_TOKEN")) {
-                    API_TOKEN = line.split("=")[1];
-                } else if (line.startsWith("API_KEY")) {
-                    API_KEY = line.split("=")[1];
-                }
-            }
-            reader.close();
+            zipFolder(this.folderPath);
+            uploadZipFile(this.folderPath);
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+            // TODO: handle exception
         }
+
     }
 
-    public void zipFolder(String folderPath) throws IOException {
+    private void zipFolder(String folderPath) throws IOException {
         // 将folderPath文件夹的所有内容打包压缩成为一个zip文件，保存在folderPath/../中
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
@@ -79,7 +102,7 @@ public class SaveUploader {
     }
 
     private void zip(ZipOutputStream zout, File target, String name) {
-        // 递归压缩target文件到zout输出流中
+        // 递归压缩target文件(文件夹)到zout输出流中
         try {
             if (target.isDirectory()) {
                 for (File file : target.listFiles()) {
@@ -101,4 +124,107 @@ public class SaveUploader {
         }
     }
 
+    private void uploadZipFile(String zipFilePath) {
+
+        COSManager cos = new COSManager(this.API_ID, this.API_KEY, this.REGION, this.BUCKET_NAME);
+        File localFile = new File(zipFilePath);
+        cos.Upload(localFile, "meta"); // 同步方法
+        localFile.delete(); // 删除本地文件
+
+    }
+
+}
+
+final class COSManager {
+
+    // COS文档https://cloud.tencent.com/document/product/436/10199
+    private String API_ID;
+    private String API_KEY;
+    private String BUCKET_NAME;
+    private String REGION_NAME;
+    private TransferManager transferManager;
+
+    COSManager(String id, String key, String bucketName, String regionName) {
+        this.API_ID = id;
+        this.API_KEY = key;
+        this.BUCKET_NAME = bucketName;
+        this.REGION_NAME = regionName;
+    }
+
+    public void Upload(File localFile, String prefix) {
+        // 使用高级接口必须先保证本进程存在一个 TransferManager 实例
+        createTransferManager(this.REGION_NAME);
+
+        // 存储桶的命名格式为 BucketName-APPID，此处填写的存储桶名称必须为此格式
+        String bucketName = this.BUCKET_NAME;
+        // 对象键(Key)是对象在存储桶中的唯一标识。
+        String key = prefix + localFile.getName();
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
+
+        try {
+            // 高级接口会返回一个异步结果Upload
+            // 可同步地调用 waitForUploadResult 方法等待上传完成，成功返回UploadResult, 失败抛出异常
+            Upload upload = this.transferManager.upload(putObjectRequest);
+            UploadResult uploadResult = upload.waitForUploadResult();
+            System.out.println(uploadResult.toString());
+        } catch (CosServiceException e) {
+            e.printStackTrace();
+        } catch (CosClientException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确定本进程不再使用 transferManager 实例之后，关闭之
+        // 详细代码参见本页：高级接口 -> 关闭 TransferManager
+        shutdownTransferManager(transferManager);
+    }
+
+    private COSClient createCOSClient(String regionName) {
+        // 1 初始化用户身份信息（secretId, secretKey）。
+        // SECRETID和SECRETKEY请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi
+        // 进行查看和管理
+        COSCredentials cred = new BasicCOSCredentials(this.API_ID, this.API_KEY);
+        // 2 设置 bucket 的地域, COS 地域的简称请参照
+        // https://cloud.tencent.com/document/product/436/6224
+        // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题
+        // Java SDK 部分。
+        Region region = new Region(regionName);
+        ClientConfig clientConfig = new ClientConfig(region);
+        // 这里建议设置使用 https 协议
+        // 从 5.6.54 版本开始，默认使用了 https
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        // 3 生成 cos 客户端。
+        COSClient cosClient = new COSClient(cred, clientConfig);
+        return cosClient;
+    }
+
+    // 创建 TransferManager 实例，这个实例用来后续调用高级接口
+    private TransferManager createTransferManager(String regionName) {
+        // 创建一个 COSClient 实例，这是访问 COS 服务的基础实例。
+        COSClient cosClient = createCOSClient(regionName);
+
+        // 自定义线程池大小，建议在客户端与 COS 网络充足（例如使用腾讯云的 CVM，同地域上传 COS）的情况下，设置成16或32即可，可较充分的利用网络资源
+        // 对于使用公网传输且网络带宽质量不高的情况，建议减小该值，避免因网速过慢，造成请求超时。
+        ExecutorService threadPool = Executors.newFixedThreadPool(32);
+
+        // 传入一个 threadpool, 若不传入线程池，默认 TransferManager 中会生成一个单线程的线程池。
+        TransferManager transferManager = new TransferManager(cosClient, threadPool);
+
+        // 设置高级接口的配置项
+        // 分块上传阈值和分块大小分别为 5MB 和 1MB
+        TransferManagerConfiguration transferManagerConfiguration = new TransferManagerConfiguration();
+        transferManagerConfiguration.setMultipartUploadThreshold(5 * 1024 * 1024);
+        transferManagerConfiguration.setMinimumUploadPartSize(1 * 1024 * 1024);
+        transferManager.setConfiguration(transferManagerConfiguration);
+
+        return transferManager;
+    }
+
+    private void shutdownTransferManager(TransferManager transferManager) {
+        // 指定参数为 true, 则同时会关闭 transferManager 内部的 COSClient 实例。
+        // 指定参数为 false, 则不会关闭 transferManager 内部的 COSClient 实例。
+        this.transferManager.shutdownNow(true);
+    }
 }
--- src/main/java/cn/ruofengx/TimeMachine/TimeMachine.java
+++ src/main/java/cn/ruofengx/TimeMachine/TimeMachine.java
@@ -1,29 +1,83 @@
 package cn.ruofengx.TimeMachine;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
+import java.io.File;
+
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 // Learn here -> https://bukkit.fandom.com/wiki/Plugin_Tutorial_(Eclipse)
 
 public final class TimeMachine extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-       getLogger().info("This is my first timemachine, it still do nothing yet.");
-       Bukkit.getPluginManager().registerEvents(this, this); //Register the events
-    }
+   private BukkitTask saveUploaderTask;
 
-    @EventHandler
-    // This event is called when a player joins the server.
-    public static void onPlayerJoin(PlayerJoinEvent event) {
-        
-       event.getPlayer().sendMessage("Welcome");
-    
-    }
+   @Override
+   public void onEnable() {
+      this.saveDefaultConfig();
+      if (getConfig().getBoolean("SaveUploader.isEnabled")) {
+         File pwd = new File(".");
+         getLogger().info("current work folder");
+         getLogger().info(pwd.getAbsolutePath());
+         for (File f : pwd.listFiles()) {
+            getLogger().info(f.getAbsolutePath());
+         }
+      }
 
+      // startSaveUploader();
+   }
 
-    
+   @Override
+   public void onDisable() {
+      // Plugin shutdown logic
+      stopSaveUploader();
+   }
+
+   @Override
+   public boolean onCommand(org.bukkit.command.CommandSender sender, org.bukkit.command.Command command, String label,
+         String[] args) {
+      if (command.getName().equalsIgnoreCase("timemachine")) {
+         if (args.length == 0) {
+            return false;
+         } else if (args[0].equalsIgnoreCase("autoupload")) {
+            if (args.length == 1) {
+               sender.sendMessage("§c请输入参数：on/off");
+               return true;
+            } else {
+               if (args[1].equalsIgnoreCase("on")) {
+                  startSaveUploader();
+               } else if (args[1].equalsIgnoreCase("off")) {
+                  stopSaveUploader();
+               } else {
+                  sender.sendMessage("§c请输入参数：on/off");
+               }
+            }
+         }
+      }
+
+      return true;
+   }
+
+   private void startSaveUploader() {
+      ConfigurationSection section = getConfig().getConfigurationSection("SaveUploader");
+      if (this.saveUploaderTask == null) {
+         this.saveUploaderTask = new SaveUploader(section).runTaskTimer(this, 200,
+               section.getInt("interval-second") * 20);
+      } else {
+         this.saveUploaderTask.cancel();
+         this.startSaveUploader();
+      }
+      ;
+   }
+
+   private void stopSaveUploader() {
+      if (this.saveUploaderTask != null) {
+         if (!this.saveUploaderTask.isCancelled()) {
+            this.saveUploaderTask.cancel();
+         }
+      }
+      this.saveUploaderTask = null;
+   }
+
 }
